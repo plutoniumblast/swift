f82754dc90ccc287e60f6a15f8247ff7c79a27a6
Manav Seksaria <vivaldi@Gojira.local>
2020-10-05 00:45:07 +0530

    SwiftUI init

---
+++ SwiftUI-SingleView/SwiftUI-SingleView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-SingleView
+//
+//  Created by Manav Seksaria on 05/10/20.
+//  Copyright © 2020 Nukes Foundation. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- iOS-blank.playground/Contents.swift
+++ iOS-blank.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var str = "Hello, playgrounds"
+var str:Character = "T";
+var str2:Int = 2;
+
+print(str);
+print(str2);
+
+
+func add(a:Int,b:Int=1) -> Int{
+    return a + b;
+}
+
+print(add(a:2))
